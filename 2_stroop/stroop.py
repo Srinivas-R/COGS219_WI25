d1abc1a53f236538fefaa3ec814bd6c81f9cc1f5
Srinivas Ravishankar <srini.shank@gmail.com>
2025-02-18 02:07:07 -0800

    Added fixation cross height 15 for .5 sec duration, 0.5 sec interval

--- 2_stroop/stroop.py
+++ 2_stroop/stroop.py
@@ -7,10 +7,20 @@ from psychopy import visual,event,core,gui
 stimuli = ['red', 'orange', 'yellow', 'green', 'blue']
 
 win = visual.Window([800,600],color="gray", units='pix',checkTiming=False)
+fixation_cross = visual.TextStim(win,text="+", height=15, color="black",pos=[0,0])
 placeholder = visual.Rect(win,width=180,height=80, fillColor="lightgray",lineColor="black", lineWidth=6,pos=[0,0])
 word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200])
 while True:
+    placeholder.draw()
+    instruction.draw()
+    fixation_cross.draw()
+    win.flip()
+    core.wait(0.5)
+    placeholder.draw()
+    instruction.draw()
+    win.flip()
+    core.wait(0.5)
     cur_stim = random.choice(stimuli)
     word_stim.setText(cur_stim)
     word_stim.setColor(cur_stim)
